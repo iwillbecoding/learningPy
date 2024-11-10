3b2819b30d95df7612ff5d3b300891f428fa89d2
hurryEson <164095590+hurryEson@users.noreply.github.com>
2024-11-10 12:04:10 +0100

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+
+# Introduction shall refer to
+# https://exercism.org/tracks/python/exercises/reverse-string
+
+messages = ["stressed", "strops", "racecar"]
+
+for message in messages:
+    print(f"{message} - {message[::-1]}")
