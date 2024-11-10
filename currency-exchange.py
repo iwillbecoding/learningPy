ba3960f02e42caa272e72bca63a6ade473e90af2
iwillbecoding <caiwendy1980@gmail.com>
2024-11-10 11:23:20 +0000

    Update currency-exchange.py
    i don't know

--- currency-exchange.py
+++ currency-exchange.py
@@ -1,3 +1,6 @@
-
-
 # Please put your solution below
+1.2USD == 1EURO
+def exchange_currency(USD,EURO):
+  return  1.2 == 1
+print(exchange_currency(2.4,2))
+#i'm not sure what to do to make it change like.
