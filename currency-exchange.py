ecc8c1d4892afa59cfb904421bf9d1aa3b01c060
iwillbecoding <caiwendy1980@gmail.com>
2024-11-10 11:35:26 +0000

    Update currency-exchange.py
    fixed with teacher

--- currency-exchange.py
+++ currency-exchange.py
@@ -1,6 +1,7 @@
 # Please put your solution below
-1.2USD == 1EURO
-def exchange_currency(USD,EURO):
-  return  1.2 == 1
-print(exchange_currency(2.4,2))
-#i'm not sure what to do to make it change like.
+
+def exchange_money(budget, exchange_rate):    
+  return budget / exchange_rate
+  
+print(exchange_money(127.5, 1.2))
+
