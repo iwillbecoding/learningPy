216fcfb06b10b1a34e66ca580757845c319f9729
hurryEson <164095590+hurryEson@users.noreply.github.com>
2024-11-17 11:55:49 +0100

    Update grains.py

--- grains.py
+++ grains.py
@@ -1,1 +1,12 @@
 
+def grains_counter(squares):
+    grains = 1
+    if squares == 1:
+        return grains
+    else:
+        for i in range(2, squares+1):
+            grains = grains * 2
+        return grains
+
+
+print(grains_counter(64))
