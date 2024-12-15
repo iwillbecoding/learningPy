1719d15fce4cb00b776c70a6bc218deb59b525cb
iwillbecoding <caiwendy1980@gmail.com>
2024-12-15 14:58:58 +0000

    i can practice

--- check_where_wrong.py
+++ check_where_wrong.py
@@ -1,5 +1,10 @@
 def add_two_numbers(number_one, number_two):
   result = number_one + number_two
-
+#each print in define more each when printing with a for loop
 
 print(add_two_numbers(5, 7))
+
+
+
+
+#just did it for future(9:30 am)
